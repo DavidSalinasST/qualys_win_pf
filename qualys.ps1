d4cb5981ec5f978c69ce66adae2962b9672faf73
DavidSalinasST <102323225+DavidSalinasST@users.noreply.github.com>
2023-02-27 17:11:45 -0300

    Update qualys.ps1

--- qualys.ps1
+++ qualys.ps1
@@ -1,2 +1,2 @@
 cd C:\Temp
-.\QualysCloudAgent.exe CustomerId={4e8468f0-df91-ee49-8015-2b5b3c382c9f} ActivationId={d2b864e2-4f0c-4d01-997f-c4adce786326} WebServiceUri=https://qagpublic.qg3.apps.qualys.com/CloudAgent/
+.\QualysCloudAgent1.exe CustomerId={4e8468f0-df91-ee49-8015-2b5b3c382c9f} ActivationId={d2b864e2-4f0c-4d01-997f-c4adce786326} WebServiceUri=https://qagpublic.qg3.apps.qualys.com/CloudAgent/
